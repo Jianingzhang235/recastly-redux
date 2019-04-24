783c824e16ded19b3ccef176ca076f72a4f2cccf
Camron Stuart <Samlennder@gmail.com>
2019-04-24 17:58:59 -0400

    Complete VideoList, ChangeVideo, HandleVideoSearch

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
+    this.getYouTubeVideos('Trappin in Japan');
   }
 
   handleVideoListEntryTitleClick(video) {
