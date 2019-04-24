b4558b60b9fe8e1b0bc35f423d6f15ce9c75326d
Camron Stuart <Samlennder@gmail.com>
2019-04-24 18:30:46 -0400

    Complete CurrentVideo reducer

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,18 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
+var currentVideoReducer = (state = null, action) => {
+  // might not need this unless there is a way for state to be undefined
+  // if(state === undefined){
+  //   return 
+  // }
+  if(action.type === "CHANGE_VIDEO"){
+       return action.video;
+  } else {
+    return state;
+  }
+
 };
+
+// this.setState(currentVideoReducer(currentState, action))
 
 export default currentVideoReducer;
